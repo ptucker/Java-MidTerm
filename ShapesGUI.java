3417972864a052d570bcd4033bea051809ce81b0
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 13:54:11 -0800

    feedback

--- ShapesGUI.java
+++ ShapesGUI.java
@@ -1,3 +1,19 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 20/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 15/15%
+Show specific shape properties correctly: 15/15%
+Well-structured, well-commented code: 15/15%
+
+
+Comments:
+  interfaceShape doesn't need to implement toString, since that's on Object
+  nicely done with the XML parser.
+*/
+
 import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
